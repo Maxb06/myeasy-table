28a163d38509a5cdef27ccbba06a986bb65c7c02
Maxime Brunet <maximb84@gmail.com>
2025-02-26 20:29:20 +0100

    Update for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    emptyOutDir: false,
+    lib: {
+      entry: "src/index.ts",
+      name: "MyEasyTable",
+      fileName: (format) => `myeasy-table.${format}.js`,
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
